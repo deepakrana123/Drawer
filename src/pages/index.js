db7f7e50f5e68cada05ab94df6fd38f86ed5d9fd
deepakrana123 <rdev69146@gmail.com>
2023-10-02 17:21:50 +0530

    toolbox

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Menu from "@/components/Menu"
+import ToolBox from '@/components/Toolbox'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <>
     <Menu/>
+    <ToolBox/>
+    </>
   )
 }
